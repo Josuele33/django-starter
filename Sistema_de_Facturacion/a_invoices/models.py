f5050d2cb710b4f6cc5c62eacafc9d4f0461b1ef
Josuele33 <josu.perez.olesa@gmail.com>
2024-07-09 18:01:23 +0200

    modificaciones

--- Sistema_de_Facturacion/a_invoices/models.py
+++ Sistema_de_Facturacion/a_invoices/models.py
@@ -8,10 +8,13 @@ class Cliente(models.Model):
     nombre    = models.CharField(max_length=31)
     apellido  = models.CharField(max_length=50)
     direccion = models.CharField(max_length=255)
-    telefono  = models.CharField(max_length=12, blank=True, null=True)
-    emial     = models.EmailField(max_length=255)
+    telefono  = models.CharField(max_length=20, blank=True, null=True)
+    email     = models.EmailField(max_length=255)
     nif       = models.CharField(max_length=12)
     tipo      = models.CharField(max_length=10, choices=TIPO_OPCIONES, default='particular')
+
+    def __str__(self) -> str:
+        return f"{self.nombre}, {self.nif}"
 
 class Factura(models.Model):
     importe   = models.DecimalField(decimal_places=2, max_digits=8)
@@ -19,3 +22,9 @@ class Factura(models.Model):
     cliente   = models.ForeignKey(Cliente, on_delete=models.DO_NOTHING)
     created_en = models.DateTimeField(auto_now_add=True)
     actualizado_en = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return f"Factura #{self.id} - {self.cliente.nombre} {self.cliente.apellido}"
+    
+    class Meta:
+        verbose_name_plural = "Facturas"
--- Sistema_de_Facturacion/a_invoices/views.py
+++ Sistema_de_Facturacion/a_invoices/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Cliente, Factura
 
-# Create your views here.
+def index(request):
+    print("Han solicitado invoices/")
+    # clientes = [1, 2, 3]
+    # facturas = ['a', 'b', 'c']
+    clientes = list(Cliente.objects.all())
+    facturas = list(Factura.objects.all())
+
+    context = {
+        "secret": "Supersafe123/",
+        "clientes": clientes,
+        "facturas": facturas,
+    }
+    return render(request, "a_invoices/index.html", context=context)
